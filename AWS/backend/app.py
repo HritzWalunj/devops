333f636e16e8782a411db5c4b4fd584198f5f084
HritzWalunj <hritujawalunj@gmail.com>
2026-02-27 13:14:27 +0530

    Run Frondend and Backend appliaction on same EC2 machine.

---
+++ AWS/backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Hrituja 👋 Your Flask app is running successfully!"
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.json or request.form
+
+    if not data.get("name") or not data.get("email"):
+        return jsonify({"error": "Name and Email are required"}), 400
+
+    # Simulating successful processing
+    print("Received Data:", data)
+
+    return jsonify({"message": "Success"}), 200
+
+app.run(host="0.0.0.0", port=5000)
